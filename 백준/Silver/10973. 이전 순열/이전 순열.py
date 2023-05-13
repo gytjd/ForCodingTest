d9902193be8405a9335a2ce3ad4ff6d74e2dfb6f
Blue <hys339631@gmail.com>
2023-05-14 00:24:59 +0900

    [Silver III] Title: 이전 순열, Time: 132 ms, Memory: 115400 KB -BaekjoonHub

---
+++ 백준/Silver/10973. 이전 순열/이전 순열.py
@@ -0,0 +1,32 @@
+import sys
+
+
+n = int(input())
+array = list(map(int, sys.stdin.readline().split()))
+
+
+k = -1
+m=0
+
+for i in range(len(array) - 1):
+    if array[i] > array[i + 1]:
+        k = i
+
+if k == -1:
+    print(-1)
+else:
+
+    for j in range(k+1,len(array)):
+        if array[k]>array[j]:
+            m=j
+
+    array[k],array[m]=array[m],array[k]
+
+    temp=array[k+1:]
+    temp.sort(reverse=True)
+    array=array[:k+1]+temp
+
+    print(*array)
+
+
+
