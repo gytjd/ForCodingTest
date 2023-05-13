1443403a6a2dd20761128c8129c03b254cdce49b
Blue <hys339631@gmail.com>
2023-05-14 00:35:50 +0900

    [Silver II] Title: N과 M (11), Time: 772 ms, Memory: 243244 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.py
@@ -0,0 +1,13 @@
+import sys
+
+from itertools import product
+
+n, m = map(int, sys.stdin.readline().split())
+array = list(map(int, sys.stdin.readline().split()))
+array.sort()
+
+array = list(product(array, repeat=m))
+array=sorted(list(set(array)))
+
+for i in array:
+    print(*i)
