e365c7edd808a812710eaaea397c992e28eb53f0
Blue <hys339631@gmail.com>
2023-05-14 00:01:58 +0900

    [Gold IV] Title: 트리의 지름, Time: 80 ms, Memory: 35572 KB -BaekjoonHub

---
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.py
@@ -0,0 +1,35 @@
+import sys
+sys.setrecursionlimit(10**7)
+n=int(input())
+
+graph=[[]for i in range(n+1)]
+
+for i in range(n-1):
+    temp=list(map(int,sys.stdin.readline().split()))
+
+    start=temp[0]
+
+    for i in range(1,len(temp)-1,+2):
+        graph[start].append((temp[i],temp[i+1]))
+        graph[temp[i]].append((start,temp[i+1]))
+
+visited=[0 for i in range(n+1)]
+result=[0 for i in range(n+1)]
+
+def dfs(start):
+    visited[start]=1
+
+    for i in graph[start]:
+        if visited[i[0]]==0:
+            result[i[0]]=result[start]+i[1]
+            dfs(i[0])
+
+dfs(1)
+temp1=max(result)
+next_node=result.index(max(result))
+visited=[0 for i in range(n+1)]
+result=[0 for i in range(n+1)]
+dfs(next_node)
+temp2=max(result)
+
+print(max(temp1,temp2))
