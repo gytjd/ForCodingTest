8a3db0822d4fb5e6cb88fe357762e0c22ac10e7d
Blue <hys339631@gmail.com>
2023-05-14 00:03:52 +0900

    [Gold V] Title: 자두나무, Time: 0 ms, Memory: 2292 KB -BaekjoonHub

---
+++ 백준/Gold/2240. 자두나무/자두나무.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+
+using namespace std;
+
+int dp[1004][2][34], n, m, b[1004];
+
+int go(int idx, int tree, int cnt){
+    if(cnt < 0) return -1e9;
+    if(idx == n) return 0;
+    int &ret = dp[idx][tree][cnt];
+    if(~ret) return ret;
+    return ret = max(go(idx + 1, tree^1, cnt - 1), go(idx + 1, tree, cnt)) + (tree == b[idx] - 1);
+}
+
+int main(){
+    memset(dp,-1,sizeof(dp));
+    cin >> n >> m;
+    for(int i = 0; i < n; i++) cin >> b[i];
+    cout << max(go(0, 1, m - 1), go(0, 0, m)) << '\n';
+    return 0;
+}
