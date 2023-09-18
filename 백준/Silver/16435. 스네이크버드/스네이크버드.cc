ca2ddf7f7de52a5ec7ce34ed24a5ca52073e7301
Blue <hys339631@gmail.com>
2023-09-18 17:25:56 +0900

    [Silver V] Title: 스네이크버드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/16435. 스네이크버드/스네이크버드.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int N,M;
+vector<int> v;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    
+    cin >> N >> M;
+    for(int i=0;i<N;i++) {
+        int data;
+        cin >> data;
+        v.push_back(data);
+    }
+    
+    sort(v.begin(), v.end());
+    
+    for(int i=0;i<N;i++) {
+        if(v[i]<=M) {
+            M+=1;
+        }
+    }
+    
+    cout << M << "\n";
+    
+    return 0;
+}
