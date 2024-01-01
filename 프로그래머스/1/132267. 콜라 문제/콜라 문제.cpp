fde8cdfaefe19d8df0caf0f241f8a6c694d01813
Blue <hys339631@gmail.com>
2024-01-01 22:53:30 +0900

    [level 1] Title: 콜라 문제, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int a, int b, int n) {
+    int answer = 0;
+    
+    while(n>=a) {
+        int temp=0;
+        answer+=(n/a)*b;
+        n=(n%a)+(n/a)*b;
+    }
+    
+    return answer;
+}
