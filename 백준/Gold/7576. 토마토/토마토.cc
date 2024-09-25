42e3ca52a874ec3675e05a3806ec5cab50f9a6e3
Blue <hys339631@gmail.com>
2024-09-25 10:08:24 +0900

    [Gold V] Title: 토마토, Time: 80 ms, Memory: 14176 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.cc
+++ 백준/Gold/7576. 토마토/토마토.cc
@@ -1,29 +1,32 @@
 #include <iostream>
 #include <queue>
+#include <algorithm>
+
 using namespace std;
 
 int N,M;
-int temp[1001][1001];
-int visited[1001][1001];
 int dir[4][2]={{0,1},{1,0},{0,-1},{-1,0}};
 
+int arr[1004][1004];
+int visited[1004][1004];
+queue<pair<int,int>> q;
 
-int BFS_iter() {
+int cal_ret() {
     
-    int cnt=0;
-    int x,y;
-    int dx,dy;
-    
-    queue<pair<int, int>> q;
+    int max_Ret=0;
     for(int i=0;i<N;i++) {
         for(int j=0;j<M;j++) {
-            if(temp[i][j]==1) {
-                q.push(make_pair(i, j));
-                visited[i][j]=1;
-                cnt=1;
+            if(visited[i][j]==0 and arr[i][j]==0) {
+                return -1;
             }
+            max_Ret=max(max_Ret,visited[i][j]);
         }
     }
+    return max_Ret-1;
+}
+void BFS_iter() {
+    int x,y;
+    int dx,dy;
     
     while(!q.empty()) {
         x=q.front().first;
@@ -34,46 +37,37 @@ int BFS_iter() {
             dx=x+dir[i][0];
             dy=y+dir[i][1];
             
-            if(dx<0 or dx>=N or dy<0 or dy>=M) {
-                continue;
+            if(dx<0 or dx>=N or dy<0 or dy>=M or arr[dx][dy]==-1) {
+                continue;;
             }
             
-            if(visited[dx][dy]==0 and temp[dx][dy]==0) {
+            if(visited[dx][dy]==0) {
                 visited[dx][dy]=visited[x][y]+1;
-                
-                if(visited[dx][dy]>cnt) {
-                    cnt=visited[dx][dy];
-                }
-                q.push(make_pair(dx, dy));
+                q.push({dx,dy});
             }
         }
     }
-    
-    
-    
-    for(int i=0;i<N;i++) {
-        for(int j=0;j<M;j++) {
-            if(visited[i][j]==0 and temp[i][j]==0) {
-                return -1;
-            }
-        }
-    }
-    return cnt-1;
 }
+
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
     
     cin >> M >> N;
-    
     for(int i=0;i<N;i++) {
         for(int j=0;j<M;j++) {
-            cin >> temp[i][j];
+            
+            cin >> arr[i][j];
+            if(arr[i][j]==1) {
+                visited[i][j]=1;
+                q.push({i,j});
+            }
         }
     }
     
-    cout << BFS_iter() << "\n";
+    BFS_iter();
+    cout << cal_ret() << "\n";
     
     return 0;
 }
