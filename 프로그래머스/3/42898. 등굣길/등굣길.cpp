c5ffb27be727336fe1889ee2e205ef21333f961a
Blue <hys339631@gmail.com>
2024-10-14 20:25:22 +0900

    [level 3] Title: 등굣길, Time: 0.07 ms, Memory: 3.79 MB -BaekjoonHub

---
+++ 프로그래머스/3/42898. 등굣길/등굣길.cpp
@@ -0,0 +1,34 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std; 
+
+int arr[104][104];
+int visited[104][104];
+
+int solution(int m, int n, vector<vector<int>> puddles) {
+    int answer = 0;
+    
+    for(vector<int> temp:puddles) {
+        arr[temp[1]][temp[0]]=1;
+    }
+    
+    visited[1][1]=1;
+    for(int i=1;i<=n;i++) {
+        for(int j=1;j<=m;j++) {
+            if(arr[i][j] or (i==1 and j==1)) continue;
+            visited[i][j]=(visited[i-1][j]+visited[i][j-1])%1000000007;
+        }
+    }
+    
+//     for(int i=1;i<=n;i++) {
+//         for(int j=1;j<=m;j++) {
+//             cout << visited[i][j] << " ";
+//         }
+//         cout << "\n";
+//     }
+    
+    
+    return visited[n][m]%1000000007;
+}
