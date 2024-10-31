4229dffcc8566690a53bf1a2b441185dab038314
Blue <hys339631@gmail.com>
2024-10-31 20:04:10 +0900

    [level 2] Title: 루시와 엘라 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
@@ -0,0 +1,2 @@
+-- 코드를 입력하세요
+select animal_id,name,sex_upon_intake from animal_ins where name="Lucy" or name="Ella" or name="Pickle" or name="Rogan" or name="Sabrina" or name="Mitty" order by animal_id
