bc82320f01b3db46c4220394040ed80c895deb43
Blue <hys339631@gmail.com>
2025-04-03 14:24:36 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
@@ -1,10 +1,11 @@
 #include <iostream>
-#include <algorithm>
+#include <vector>
+
 using namespace std;
 
-int N,ret;
-int arr[1004];
-int dp[1004];
+int N;
+vector<int> v;
+vector<int> dp;
 
 int main() {
     ios_base::sync_with_stdio(false);
@@ -12,21 +13,23 @@ int main() {
     cout.tie(nullptr);
     
     cin >> N;
+    for(int i=0;i<N;i++) {
+        int data;
+        cin >> data;
+        v.push_back(data);
+    }
     
     for(int i=0;i<N;i++) {
-        cin >> arr[i];
-    }
-    
-    ret=dp[0];
-    for(int i=1;i<N;i++) {
-        for(int j=0;j<i;j++) {
-            if(arr[i]>arr[j]) {
-                dp[i]=max(dp[j]+1,dp[i]);
-                ret=max(dp[i],ret);
-            }
+        
+        if(dp.empty() or dp.back()<v[i]) {
+            dp.push_back(v[i]);
+        } else {
+            int lb=lower_bound(dp.begin(), dp.end(), v[i])-dp.begin();
+//            cout << i << " " << lb << "\n";
+            dp[lb]=v[i];
         }
     }
-    cout << ret+1 << "\n";
-
+    cout << dp.size() << "\n";
+    
     return 0;
 }
