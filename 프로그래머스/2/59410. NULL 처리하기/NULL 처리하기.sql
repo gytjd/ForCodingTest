2c5f895abec9ac7765542565e86eb745a390c962
Blue <hys339631@gmail.com>
2025-05-16 00:18:56 +0900

    [level 2] Title: NULL 처리하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59410. NULL 처리하기/NULL 처리하기.sql
+++ 프로그래머스/2/59410. NULL 처리하기/NULL 처리하기.sql
@@ -1,2 +1,2 @@
 -- 코드를 입력하세요
-select animal_type,case when name is null then "No name" else name end as name,sex_upon_intake from animal_ins
+SELECT i.animal_type as ANIMAL_TYPE,case when i.name is null then "No name" else i.name end as NAME,i.sex_upon_intake as SEX_UPON_INTAKE from animal_ins i
