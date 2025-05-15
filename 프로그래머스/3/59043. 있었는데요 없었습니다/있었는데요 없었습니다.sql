d808dce91e2adadcdd86352490ebad793672692e
Blue <hys339631@gmail.com>
2025-05-16 00:23:28 +0900

    [level 3] Title: 있었는데요 없었습니다, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
+++ 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
@@ -1,2 +1,2 @@
 -- 코드를 입력하세요
-select i.animal_id,i.name from animal_ins i join animal_outs o on i.animal_id=o.animal_id where i.datetime > o.datetime order by i.datetime
+SELECT i.animal_id as ANIMAL_ID,i.name as NAME from animal_ins i join animal_outs o on i.animal_id=o.animal_id where i.datetime>o.datetime order by i.datetime
