e1a41945329a20aaef6b31cd23d856c03cc3993f
Blue <hys339631@gmail.com>
2025-07-25 14:35:48 +0900

    [Silver III] Title: 퇴사, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.cc
+++ 백준/Silver/14501. 퇴사/퇴사.cc
@@ -6,35 +6,41 @@ using namespace std;
 
 int N;
 int ret;
-vector<pair<int,int>> v;
+vector<pair<int, int>> v;
+int visited[20];
 
-void DFS_recur(int curr,int num) {
-    
-    ret=max(ret,num);
-    if(curr>N) {
-        return;
-    }
-    
-    for(int i=curr;i<N;i++) {
-        if(i+v[i].first<=N) {
-            DFS_recur(i+v[i].first, num+v[i].second);
-        }
-    }
-}
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-    
-    cin >> N;
-    for(int i=0;i<N;i++) {
-        int tim,data;
-        cin >> tim >> data;
-        v.push_back({tim,data});
-    }
-    
-    DFS_recur(0, 0);
-    cout << ret << "\n";
-    
-    return 0;
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	cin >> N;
+
+	v.push_back({ 0,0 });
+	for (int i = 0; i < N; i++) {
+		int T, P;
+		cin >> T >> P;
+		v.push_back({ T,P });
+	}
+
+	for (int i = 1; i <= N; i++) {
+
+		if (v[i].first + i > N+1) {
+			continue;
+		}
+
+		int temp = 0;
+		for (int j = 0; j < i; j++) {
+			if (v[j].first + j <= i) {
+				temp = max(temp, visited[j]);
+			}
+		}
+		visited[i] = temp + v[i].second;
+		ret = max(ret, visited[i]);
+	}
+
+	cout << ret << "\n";
+
+	return 0;
 }
