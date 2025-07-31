71b6e6c5ac1f05b7f946e292967f96cf1c4bb15d
Blue <hys339631@gmail.com>
2025-07-31 11:51:20 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.cc
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.cc
@@ -4,55 +4,68 @@
 
 using namespace std;
 
-int N;
-vector<int> v;
-int op[4];
-long long max_Ret=-10999999999;
-long long min_Ret=10999999999;
+long long N;
+long long maxRet = -10999999999;
+long long minRet = 10999999999;
 
-void DFS_recur(int cnt,long long num) {
-    if(cnt==N-1) {
-        max_Ret=max(max_Ret,num);
-        min_Ret=min(min_Ret,num);
-        return;
-    }
-    
-    for(int i=0;i<4;i++) {
-        if(op[i]) {
-            op[i]-=1;
-            if(i==0) {
-                DFS_recur(cnt+1, num+v[cnt+1]);
-            }
-            else if(i==1) {
-                DFS_recur(cnt+1, num-v[cnt+1]);
-            }
-            else if(i==2) {
-                DFS_recur(cnt+1, num*v[cnt+1]);
-            }
-            else {
-                DFS_recur(cnt+1, num/v[cnt+1]);
-            }
-            op[i]+=1;
-        }
-    }
+vector<long long> v;
+vector<long long> com;
+
+void go_(long long curr,long long Num) {
+
+	if (curr == N) {
+		maxRet = max(maxRet, Num);
+		minRet = min(minRet, Num);
+		return;
+	}
+
+	if (com[0]) {
+		com[0] -= 1;
+		go_(curr + 1, Num + v[curr]);
+		com[0] += 1;
+	}
+
+	if (com[1]) {
+		com[1] -= 1;
+		go_(curr + 1, Num - v[curr]);
+		com[1] += 1;
+	}
+
+	if (com[2]) {
+		com[2] -= 1;
+		go_(curr + 1, Num * v[curr]);
+		com[2] += 1;
+	}
+
+	if (com[3]) {
+		com[3] -= 1;
+		go_(curr + 1, Num / v[curr]);
+		com[3] += 1;
+	}
 }
+
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-    
-    cin >> N;
-    for(int i=0;i<N;i++) {
-        int data;
-        cin >> data;
-        v.push_back(data);
-    }
-    for(int i=0;i<4;i++) {
-        cin >> op[i];
-    }
-    DFS_recur(0, v[0]);
-    cout << max_Ret << "\n";
-    cout << min_Ret << "\n";
-    
-    return 0;
+	
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	cin >> N;
+	for (long long i = 0; i < N; i++) {
+		long long data;
+		cin >> data;
+		v.push_back(data);
+	}
+
+	for (long long i = 0; i < 4; i++) {
+		long long data;
+		cin >> data;
+		com.push_back(data);
+	}
+
+	go_(1, v[0]);
+	cout << maxRet << "\n";
+	cout << minRet << "\n";
+
+	return 0;
 }
