b95bdacac79685f73d460595b2d8521b6ffaa22a
Blue <hys339631@gmail.com>
2025-08-01 10:03:05 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.cc
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.cc
@@ -1,135 +1,49 @@
 #include <iostream>
-#include <queue>
+#include <cstring>
+
 using namespace std;
 
 int N;
-int arr[20][20];
-int dir[3][2]={{0,1},{1,1},{1,0}};
 int ret;
-
-struct Node {
-    int curr;
-    int x,y;
-};
-
-
-void DFS_recur(int curr,int x,int y) {
-    
-    
-    int dx,dy;
-    
-    if(x==N-1 and y==N-1) {
-        ret+=1;
-        return;
-    }
-    
-    
-    if(curr==0) {
-        
-        for(int i=0;i<2;i++) {
-            bool check_Flag=false;
-            dx=x+dir[i][0];
-            dy=y+dir[i][1];
-            
-            if(dx<0 or dx>=N or dy<0 or dy>=N) {
-                continue;
-            }
-            
-            if(i==1) {
-                for(int j=0;j<3;j++) {
-                    if(arr[x+dir[j][0]][y+dir[j][1]]==1) {
-                        check_Flag=true;
-                        break;
-                    }
-                }
-            }
-            else {
-                if(arr[dx][dy]==1) {
-                    check_Flag=true;
-                }
-            }
-            
-            if(!check_Flag) {
-                DFS_recur(i, dx, dy);
-            }
-        }
-    }
-    else if(curr==1) {
-        
-        for(int i=0;i<3;i++) {
-            bool check_Flag=false;
-            dx=x+dir[i][0];
-            dy=y+dir[i][1];
-            if(dx<0 or dx>=N or dy<0 or dy>=N) {
-                continue;
-            }
-            
-            if(i==1) {
-                for(int j=0;j<3;j++) {
-                    if(arr[x+dir[j][0]][y+dir[j][1]]==1) {
-                        check_Flag=true;
-                        break;
-                    }
-                }
-            }
-            else {
-                if(arr[dx][dy]==1) {
-                    check_Flag=true;
-                }
-            }
-            
-            if(!check_Flag) {
-                DFS_recur(i, dx, dy);
-            }
-        }
-    }
-    else if(curr==2) {
-        
-        for(int i=1;i<3;i++) {
-            bool check_Flag=false;
-            dx=x+dir[i][0];
-            dy=y+dir[i][1];
-            if(dx<0 or dx>=N or dy<0 or dy>=N) {
-                continue;
-            }
-            
-            if(i==1) {
-                for(int j=0;j<3;j++) {
-                    if(arr[x+dir[j][0]][y+dir[j][1]]==1) {
-                        check_Flag=true;
-                        break;
-                    }
-                }
-            }
-            else {
-                if(arr[dx][dy]==1) {
-                    check_Flag=true;
-                }
-            }
-            
-            if(!check_Flag) {
-                DFS_recur(i, dx, dy);
-            }
-        }
-    }
-}
+int arr[20][20];
+int visited[3][20][20];
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-    
-    cin >> N;
-    
-    for(int i=0;i<N;i++) {
-        for(int j=0;j<N;j++) {
-            cin >> arr[i][j];
-        }
-    }
-    
-    DFS_recur(0, 0, 1);
-    
-    cout << ret << "\n";
-    
-    return 0;
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	memset(arr, 1, sizeof(arr));
+
+	cin >> N;
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= N; j++) {
+			cin >> arr[i][j];
+		}
+	}
+
+	visited[0][1][2] = 1;
+
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= N; j++) {
+
+			if (i == 1 and (j == 1 or j==2)) continue;
+			if (arr[i][j]==0) {
+				visited[0][i][j] = visited[0][i][j - 1] + visited[2][i][j - 1];
+				visited[1][i][j] = visited[1][i - 1][j] + visited[2][i - 1][j];
+			}
+
+			if (arr[i][j] == 0 and arr[i][j - 1] == 0 and arr[i - 1][j] == 0) {
+				visited[2][i][j] = visited[0][i - 1][j - 1] + visited[1][i - 1][j - 1] + visited[2][i - 1][j - 1];
+			}
+		}
+	}
+
+	for (int i = 0; i < 3; i++) {
+		ret += visited[i][N][N];
+	}
+
+	cout << ret << "\n";
+
+	return 0;
 }
