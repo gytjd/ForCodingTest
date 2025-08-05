4fe94e904e104f396f003ab4dc710ca422d548e4
Blue <hys339631@gmail.com>
2025-08-05 19:02:49 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 112 ms, Memory: 6148 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
@@ -2,47 +2,47 @@
 
 using namespace std;
 
-int temp[1028][1028];
+int N, M;
+int arr[1028][1028];
+
+void init_() {
+
+	for (int i = 1; i <= N; i++) {
+		for (int j = 2; j <= N; j++) {
+			arr[i][j] += arr[i][j - 1];
+		}
+	}
+
+	for (int i = 1; i <= N; i++) {
+		for (int j = 2; j <= N; j++) {
+			arr[j][i] += arr[j - 1][i];
+		}
+	}
+}
 
 
 int main() {
-    
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    
-    int i,j;
-    int N,M;
-    
-    int tempX,tempY;
-    int oriX,oriY;
-    cin >> N >> M;
-    
-    for(i=1;i<=N;i++) {
-        for(j=1;j<=N;j++) {
-            cin >> temp[i][j];
-        }
-    }
-    
-    for(i=1;i<=N;i++) {
-        for(j=1;j<=N;j++) {
-            temp[i][j]+=temp[i-1][j];
-        }
-    }
-    
-    for(i=1;i<=N;i++) {
-        for(j=1;j<=N;j++) {
-            temp[i][j]+=temp[i][j-1];
-        }
-    }
-    
-    for(i=0;i<M;i++) {
-        cin >> tempX >> tempY >> oriX >> oriY;
-        
-        cout << temp[oriX][oriY]-temp[tempX-1][oriY]-temp[oriX][tempY-1]+temp[tempX-1][tempY-1] << "\n";
-    }
-    
-    
-    return 0;
-    
-}
 
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	cin >> N >> M;
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= N; j++) {
+			cin >> arr[i][j];
+		}
+	}
+
+	init_();
+
+	for (int i = 0; i < M; i++) {
+		int startX, startY, endX, endY;
+		cin >> startX >> startY >> endX >> endY;
+
+		cout << arr[endX][endY] - arr[endX][startY - 1] - arr[startX - 1][endY] + arr[startX - 1][startY - 1] << "\n";
+	}
+
+
+	return 0;
+}
