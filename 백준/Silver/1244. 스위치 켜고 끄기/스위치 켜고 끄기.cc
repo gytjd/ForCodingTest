a8cee468509c706d41dbdcde74e8a3e1a19c1689
Blue <hys339631@gmail.com>
2025-08-13 10:50:17 +0900

    [Silver IV] Title: 스위치 켜고 끄기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.cc
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.cc
@@ -4,63 +4,68 @@
 using namespace std;
 
 int N,M;
-int arr[104];
+vector<int> v;
 
 void display_() {
-    for(int i=1;i<=N;i++) {
-        cout << arr[i] << " ";
-        if(i%20==0) cout << "\n";
+    
+    int tempCnt=0;
+    for(int i=0;i<N;i++) {
+        cout << v[i] << " ";
+        tempCnt+=1;
+        
+        if(tempCnt==20) {
+            cout << "\n";
+            tempCnt=0;
+        }
     }
     cout << "\n";
 }
 
-void man_go(int temp) {
+void boy_(int idx) {
     
-    for(int i=temp;i<=N;i+=temp) {
-        arr[i]=!arr[i];
+    for(int i=idx;i<N;i+=(idx+1)) {
+        v[i]=!v[i];
     }
 }
 
-void girl_go(int temp) {
-    int low=temp;
-    int high=temp;
+void girl_(int idx) {
     
-    while(low>0 and high<=N) {
-        if(arr[low]!=arr[high]) {
+    int low=idx-1;
+    int high=idx+1;
+    
+    v[idx]=!v[idx];
+    while(low>=0 and high<N) {
+        
+        if(v[low]==v[high]) {
+            v[low]=!v[low];
+            v[high]=!v[high];
+        } else {
             break;
         }
-        low-=1;
-        high+=1;
+        low-=1; high+=1;
     }
-    
-//    cout << low << " " << high << "\n";
-    for(int i=low+1;i<high;i++) {
-        arr[i]=!arr[i];
-    }
-    
 }
 
 int main() {
+    
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
     
     cin >> N;
-    for(int i=1;i<=N;i++) {
-        cin >> arr[i];
+    for(int i=0;i<N;i++) {
+        int data;
+        cin >> data;
+        v.push_back(data);
     }
     
     cin >> M;
     for(int i=0;i<M;i++) {
-        int com,num;
-        cin >> com >> num;
+        int com,idx;
+        cin >> com >> idx;
         
-        if(com==1) {
-            man_go(num);
-        }
-        else {
-            girl_go(num);
-        }
+        if(com==1) boy_(idx-1);
+        else girl_(idx-1);
     }
     display_();
     
