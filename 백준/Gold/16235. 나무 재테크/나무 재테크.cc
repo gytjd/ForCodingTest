c2b950ab4e240ba3a62b70f8e920e13e4f14f3c9
Blue <hys339631@gmail.com>
2025-08-14 09:46:32 +0900

    [Gold III] Title: 나무 재테크, Time: 208 ms, Memory: 2720 KB -BaekjoonHub

--- 백준/Gold/16235. 나무 재테크/나무 재테크.cc
+++ 백준/Gold/16235. 나무 재테크/나무 재테크.cc
@@ -1,94 +1,122 @@
 #include <iostream>
-#include <algorithm>
-#include <queue>
+#include <deque>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
-struct Node {
-    int x,y,weight;
-};
-
 int N,M,K;
-int arr[14][14];
-int temp[14][14];
-int dir[8][2]={{0,1},{1,1},{1,0},{1,-1},{0,-1},{-1,-1},{-1,0},{-1,1}};
+int A[14][14];
+int curr[14][14];
+int dir[8][2]={{-1,1},{0,1},{1,1},{1,0},{1,-1},{0,-1},{-1,-1},{-1,0}};
 
 deque<int> q[14][14];
-vector<Node> alive;
-vector<Node> dead;
+vector<int> alive[14][14];
+vector<int> dead[14][14];
 
+void display_() {
+    
+    for(int i=1;i<=N;i++) {
+        for(int j=1;j<=N;j++) {
+            cout << curr[i][j] << " ";
+        }
+        cout << "\n";
+    }
+    cout << "\n";
+}
+
+void init_() {
+    
+    for(int i=1;i<=N;i++) {
+        for(int j=1;j<=N;j++) {
+            curr[i][j]=5;
+        }
+    }
+}
 
 void spring_() {
-    alive.clear();
-    dead.clear();
-    int temp;
     
-    for(int i=0;i<N;i++) {
-        for(int j=0;j<N;j++) {
-            if(q[i][j].size()!=0) {
-                sort(q[i][j].begin(), q[i][j].end());
-                
-                while(!q[i][j].empty()) {
-                    temp=q[i][j].front();
-                    q[i][j].pop_front();
-                    
-                    if(temp<=arr[i][j]) {
-                        arr[i][j]-=temp;
-                        temp+=1;
-                        alive.push_back({i,j,temp});
-                    }
-                    else {
-                        dead.push_back({i,j,temp});
-                    }
-                }
-            }
+    for(int i=1;i<=N;i++) {
+        for(int j=1;j<=N;j++) {
             
+            sort(q[i][j].begin(),q[i][j].end());
+            while(!q[i][j].empty()) {
+                
+                if(curr[i][j]>=q[i][j].front()) {
+                    curr[i][j]-=q[i][j].front();
+                    alive[i][j].push_back(q[i][j].front()+1);
+                } else {
+                    dead[i][j].push_back(q[i][j].front());
+                }
+                q[i][j].pop_front();
+            }
         }
     }
 }
 
 void summer_() {
     
-    for(Node temp:dead) {
-        arr[temp.x][temp.y]+=temp.weight/2;
+    for(int i=1;i<=N;i++) {
+        for(int j=1;j<=N;j++) {
+            
+            for(int temp:dead[i][j]) {
+                curr[i][j]+=(temp/2);
+            }
+            dead[i][j].clear();
+        }
     }
 }
 
 void fall_() {
     
-    int x,y;
-    int dx,dy;
-    
-    for(Node temp:alive) {
-        q[temp.x][temp.y].push_back(temp.weight);
-
-        if(temp.weight%5==0) {
-            x=temp.x;
-            y=temp.y;
+    for(int i=1;i<=N;i++) {
+        for(int j=1;j<=N;j++) {
             
-            for(int i=0;i<8;i++) {
-                dx=x+dir[i][0];
-                dy=y+dir[i][1];
+            for(int temp:alive[i][j]) {
                 
-                if(dx<0 or dx>=N or dy<0 or dy>=N) {
-                    continue;
+                q[i][j].push_back(temp);
+                if(temp%5==0) {
+                    
+                    for(int k=0;k<8;k++) {
+                        int dx=i+dir[k][0];
+                        int dy=j+dir[k][1];
+                        
+                        if(dx<=0 or dx>N or dy<=0 or dy>N) {
+                            continue;
+                        }
+                        
+                        q[dx][dy].push_back(1);
+                    }
                 }
-                
-                q[dx][dy].push_back(1);
             }
+            alive[i][j].clear();
         }
     }
 }
 
 void winter_() {
-    for(int i=0;i<N;i++) {
-        for(int j=0;j<N;j++) {
-            arr[i][j]+=temp[i][j];
+    
+    for(int i=1;i<=N;i++) {
+        for(int j=1;j<=N;j++) {
+            curr[i][j]+=A[i][j];
         }
     }
 }
+
+int cal_() {
+    
+    int ret=0;
+    for(int i=1;i<=N;i++) {
+        for(int j=1;j<=N;j++) {
+            ret+=q[i][j].size();
+        }
+    }
+    
+    return ret;
+}
 void go_() {
+    
+    init_();
     
     for(int i=0;i<K;i++) {
         spring_();
@@ -97,37 +125,30 @@ void go_() {
         winter_();
     }
     
-    int result=0;
-    for(int i=0;i<N;i++) {
-        for(int j=0;j<N;j++) {
-            result+=q[i][j].size();
-        }
-    }
-    
-    cout << result << "\n";
+    cout << cal_() << "\n";
 }
+
 int main() {
+    
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
     
     cin >> N >> M >> K;
     
-    for(int i=0;i<N;i++) {
-        for(int j=0;j<N;j++) {
-            cin >> temp[i][j];
-            arr[i][j]=5;
+    for(int i=1;i<=N;i++) {
+        for(int j=1;j<=N;j++) {
+            cin >> A[i][j];
         }
     }
     
     for(int i=0;i<M;i++) {
-        int x,y,weight;
-        cin >> x >> y >> weight;
-        q[x-1][y-1].push_back(weight);
+        int x,y,o;
+        cin >> x >> y >> o;
+        q[x][y].push_back(o);
     }
     
     go_();
-
+    
     return 0;
 }
-
